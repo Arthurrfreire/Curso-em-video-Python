c33b6d2136beed2bc75bf870bd980b4d899d97d7
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-09-21 18:03:57 -0300

    mais correções

--- curso-python/Exercicio 6.py
+++ curso-python/Exercicio 6.py
@@ -1,7 +1,4 @@
 #Crie um algoritimo que leia um número e mostre o seu dobro, triplo e raiz quadrada
 
-numero = int(input('Digite um número: '))
-dobro = numero * 2
-triplo = numero * 3
-raiz = numero **(1/2)
-print('O dobro do número {} é {}, o triplo é {} e a raiz quadrada é {:.2f} '.format(numero, dobro, triplo, raiz))
+n = int(input('Digite um número: '))
+print('O dobro do número {} é {}, o triplo é {} e a raiz quadrada é {:.2f} '.format(n, (n*2), (n*3), (n**(1/2))))
