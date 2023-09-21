1caf37fb6d55b8be9e3858de639e30c2cdc6b62d
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-09-21 18:00:04 -0300

    mias correções

--- curso-python/Exercicio 6.py
+++ curso-python/Exercicio 6.py
@@ -3,5 +3,5 @@
 numero = int(input('Digite um número: '))
 dobro = numero * 2
 triplo = numero * 3
-raiz = numero **2
-print('O dobro do número {} é {}, o triplo é {} e a raiz quadrada é {} '.format(numero, dobro, triplo, raiz))
+raiz = numero **(1/2)
+print('O dobro do número {} é {}, o triplo é {} e a raiz quadrada é {:.2f} '.format(numero, dobro, triplo, raiz))
