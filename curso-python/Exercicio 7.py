f962d3432d3786e62107f3e14db84f01e20e8a90
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-09-21 18:07:30 -0300

    mias correções

--- curso-python/Exercicio 7.py
+++ curso-python/Exercicio 7.py
@@ -2,4 +2,4 @@
 
 nota1 = float(input('Digite a primeira nota do aluno: '))
 nota2 = float(input('Digite a segunda nota do aluno: '))
-print('A media do aluno é {} '.format((nota1 + nota2) / 2))
+print('A media do aluno é {:.1f} '.format((nota1 + nota2) / 2))
