f918f3e22d5fb79a6bb0c6c4f5853293d8734a19
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-09-21 18:14:05 -0300

    mais coisas

--- curso-python/Exercicio 10.py
+++ curso-python/Exercicio 10.py
@@ -1,5 +1,4 @@
 #Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar. Considere US$ = R$ 3,27
 
-dinheiro = float(input('Digite quanto você tem em dinheiro agora: R$ '))
-dolar = dinheiro / 3.27
-print('Com o dinheiro informado é possivel comprar US$ {:.2f}'.format(dolar))
+dinheiro = float(input('Quanto dinheiro você tem na carteira? R$ '))
+print('Com o dinheiro informado é possivel comprar US$ {:.2f}'.format(dinheiro / 3.27))
