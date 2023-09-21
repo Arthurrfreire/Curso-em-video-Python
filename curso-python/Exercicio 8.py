51a24e912b66df905cc756695c1ffa4bf5402962
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-09-21 18:09:15 -0300

    Update Exercicio 8.py

--- curso-python/Exercicio 8.py
+++ curso-python/Exercicio 8.py
@@ -1,6 +1,4 @@
 #Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros
 
 valor = int(input('Digite um valor em metros: '))
-cem = valor * 100
-mil = valor * 1000
-print('O valor {} metros convertidos em centímetros é {} cm e convertido em milímetros é {} mm '.format(valor, cem, mil))
+print('O valor {} metros convertidos em centímetros é {} cm e convertido em milímetros é {} mm '.format(valor, (valor * 100), (valor * 1000)))
