357d6c0c437d6c0b62d001a9684b7fb279a6e253
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-09-21 18:26:25 -0300

    mias

--- curso-python/Exercicio 13.py
+++ curso-python/Exercicio 13.py
@@ -1,7 +1,5 @@
 #Faça um algoritimo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
 
 salario = float(input('Digite seu salário: R$ '))
-percentual = 15
-aumento = (salario * percentual) / 100
-novo = aumento + salario
+novo = salario + (salario * 15 / 100)
 print('Seu salário com 15% de aumento é R$ {:.2f} '.format(novo))
