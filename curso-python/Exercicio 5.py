aa61b5f8b969e9f39b1b203d4f827f5f44ca3412
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-09-21 17:55:59 -0300

    correções

--- curso-python/Exercicio 5.py
+++ curso-python/Exercicio 5.py
@@ -1,6 +1,4 @@
 #Faça um programa que lei um número inteiro e mostre na tela o seu sucessor e seu antecessor
 
-numero = int(input('Digite um número: '))
-antecessor = numero - 1
-sucessor = numero + 1
-print('O antecessor de {} é {} e o sucessor é {} '.format(numero, antecessor, sucessor))
+n = int(input('Digite um número: '))
+print('O antecessor de {} é {} e o sucessor é {} '.format(n, (n-1), (n-2)))
