4aebb075035cc052c97d4363fdf4c737504b86ef
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-09-21 18:23:54 -0300

    tem mais

--- curso-python/Exercicio 12.py
+++ curso-python/Exercicio 12.py
@@ -1,6 +1,5 @@
 #Faça um algoritimo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
-preco = float(input('Digite o valor do produto: '))
-desconto = preco * 0.05
-novo = preco - desconto
+preço = float(input('Digite o valor do produto: R$ '))
+novo = preço - (preço * 0.05)
 print('O novo valor do produto com 5% de desconto é R$ {:.2f} '.format(novo))
