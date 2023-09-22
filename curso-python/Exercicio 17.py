b82a78bcced2cb0e3fa958d7d9e856f7c40545e9
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-09-22 18:37:07 -0300

    uma

--- curso-python/Exercicio 17.py
+++ curso-python/Exercicio 17.py
@@ -1,7 +1,7 @@
 #Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa.
 
 import math
-oposto = int(input('Digite o Seno: '))
-adjacente = int(input('Digite o Cosseno: '))
+oposto = float(input('Digite o Seno: '))
+adjacente = float(input('Digite o Cosseno: '))
 hipo = math.sqrt(oposto**2 + adjacente**2)
 print('O comprimento da hipotenusa é: {:.2f}'.format(hipo))
