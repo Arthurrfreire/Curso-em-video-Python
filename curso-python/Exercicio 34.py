1ae5a640b1a88abb932089c96a16a6eda4377e25
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-10-02 21:20:20 -0300

    Update Exercicio 34.py

--- curso-python/Exercicio 34.py
+++ curso-python/Exercicio 34.py
@@ -3,9 +3,9 @@ Para salários superiores a R$1.250,00, calcule um aumento de 10%.
 Para os inferiores ou iguais, o aumento é de 15%'''
 
 salario = float(input('Digite o valor do seu salário: R$ '))
-aumento_1 = salario + 0.10 * salario
 if salario > 1250.00:
-    print('Seu salário com aumento de 15% é de R$ {:.2f}'.format(aumento_1))
+    aumento = salario + 0.10 * salario
+    print('Seu salário com aumento de 15% é de R$ {:.2f}'.format(aumento))
 else:
-    aumento_2 = salario + 0.15 * salario
-    print('Seu salário com 10% de aumento é de {:.2f}'.format(aumento_2))
+    aumento = salario + 0.15 * salario
+    print('Seu salário com 10% de aumento é de {:.2f}'.format(aumento))
