0a8c5e2f850dab0502b5f1f0cc71a9c8c38bc552
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-10-05 19:37:53 -0300

    update

--- curso-python/Exercicio 37.py
+++ curso-python/Exercicio 37.py
@@ -4,15 +4,18 @@
 2 para octal
 3 para hexadecimal'''
 
-num = int(input('Digite um número: '))
-base = int(input('Em qual base será convertido:\n[ 1 ] para binário\n[ 2 ] para octal\n[ 3 ] para hexadecimal\nSua opção: '))
-if base == 1:
-    b = bin(num)
-    print('O número {} convertido para binário é {}'.format(num, b))
-elif base == 2:
-    o = oct(num)
-    print('O número {} convertido para octal é {}'.format(num, o))
-elif base == 3:
-    h = hex(num)
-    print('O número {} convertido para hexadecimal é {}'.format(num, h))
+num = int(input('Digite um número inteiro: '))
+print('''Em qual base será convertido:
+[ 1 ] para binário
+[ 2 ] para octal
+[ 3 ] para hexadecimal''')
+opção = int(input('Sua opção: '))             
+if opção == 1:
+    print('O número {} convertido para binário é {}'.format(num, bin(num) [2:]))
+elif opção == 2:
+    print('O número {} convertido para octal é {}'.format(num, oct(num) [2:]))
+elif opção == 3:
+    print('O número {} convertido para hexadecimal é {}'.format(num, hex(num) [2:]))
+else:
+    print('Opção inválida. Tente novamente')
      
