10450dbdffa448398c6cd3317b7eec753b30d5b2
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-10-05 19:25:01 -0300

    respostas

--- curso-python/Exercicio 36.py
+++ curso-python/Exercicio 36.py
@@ -2,3 +2,13 @@
 
 Calcule o valor da prestação mensal sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado.'''
 
+casa = float(input('Qual o valor da casa: R$ '))
+salario = float(input('Qual o seu salário: R$ '))
+anos = int(input('Em quantos anos quer pagar? '))
+meses = anos * 12
+prestação = casa / meses
+if prestação <= 0.3 * salario:
+    print('O valor da prestação mensal é de {:.2f}'.format(prestação))
+else:
+    print('Para pagar uma casa de R${:.2f} em {} anos a prestação será de R${:.2f}'.format(casa, anos , prestação))
+print('Empréstimo NEGADO')
--- curso-python/Exercicio 37.py
+++ curso-python/Exercicio 37.py
@@ -4,3 +4,15 @@
 2 para octal
 3 para hexadecimal'''
 
+num = int(input('Digite um número: '))
+base = int(input('Em qual base será convertido:\n[ 1 ] para binário\n[ 2 ] para octal\n[ 3 ] para hexadecimal\nSua opção: '))
+if base == 1:
+    b = bin(num)
+    print('O número {} convertido para binário é {}'.format(num, b))
+elif base == 2:
+    o = oct(num)
+    print('O número {} convertido para octal é {}'.format(num, o))
+elif base == 3:
+    h = hex(num)
+    print('O número {} convertido para hexadecimal é {}'.format(num, h))
+     
