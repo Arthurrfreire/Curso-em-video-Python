5a083ef53178b9bc748089a6c2fecc9e46d1181c
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-10-05 20:29:10 -0300

    correções

--- curso-python/Exercicio 38.py
+++ curso-python/Exercicio 38.py
@@ -1,6 +1,14 @@
-'''Escreva um programa que leia dos números inteiros e compare-os, mostrando na tela uma mensagem:
+'''Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela uma mensagem:
 
 O primeiro valor é maior
 o segundo valor é maior
 Não existe valor maior, os dois são iguais'''
 
+a = int(input('Digite um número: '))
+b = int(input('Digite outro número: '))
+if b < a:
+    print('O primeiro valor é maior')
+elif a < b:
+    print('O segundo valor é maior')
+else:
+    print('Não existe valor maior, os dois são iguais')
