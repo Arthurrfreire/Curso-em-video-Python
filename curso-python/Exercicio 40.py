fbe4d59b2ead46426a0caf2119fb711b0a1b8cb8
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-10-07 10:17:35 -0300

    updata

--- curso-python/Exercicio 40.py
+++ curso-python/Exercicio 40.py
@@ -3,3 +3,13 @@
 - Média abaixo de 5.0: REPROVADO
 - Média entre 5.0 e 6.9: RECUPERAÇÃO
 - Média 7.0 ou superior: APROVADO'''
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+media = (nota1 + nota2) / 2
+if media < 5.0:
+    print('Sua média é {:.2f}, portanto você foi REPROVADO'.format(media))
+elif 5.0 <= media <= 6.9:
+    print('Sua média é {:.2f}, portanto você vai para a RECUPERAÇÃO'.format(media))
+else:
+    print('Sua média é {:.2f}, portanto você foi APROVADO'.format(media))
--- curso-python/Exercicio 41.py
+++ curso-python/Exercicio 41.py
@@ -5,3 +5,18 @@
 - Até 19 anos: JUNIOR
 - Até 20 anos: SÊNIOR
 - Acima: Master'''
+
+from datetime import date
+atual = date.today().year
+ano = int(input('Digite o ano de nascimento do atleta: '))
+idade = atual - ano
+if idade <= 9:
+    print('Com a idade de {} anos o atleta é da categoria MIRIM'.format(idade))
+elif 9 < idade <= 14:
+    print('Com a idade de {} anos o atleta é da categoria INFANTIL'.format(idade))
+elif 14 < idade <= 19:
+    print('Com a idade de {} anos o atleta é da categoria JUNIOR'.format(idade))
+elif 19 < idade <= 20:
+    print('Com a idade de {} anos o atleta é da categoria SÊNIOR'.format(idade))
+else:
+    print('Com a idade de {} anos o atleta é da categoria MASTER'.format(idade))
