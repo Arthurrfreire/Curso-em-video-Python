726685bb19fd44fc4ea1dfef7bc1d4b309e908c5
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-10-09 21:07:25 -0300

    Update Exercicio 45.2.py

---
+++ curso-python/Exercicio 45.2.py
@@ -0,0 +1,56 @@
+from random import randint
+from time import sleep
+
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+
+print('''Suas opções:
+[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA''')
+
+#print('O computador escolheu {}'.format(itens[computador])) para o computador randomizar o conteúdo da variavél itens.
+
+jogador = int(input('Qual é a sua jogada? '))
+
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PÔ')
+sleep(1)
+
+print('-=' * 15)
+print('O computador jogou {}'.format(itens[computador]))
+print('O jogador jogou {}'.format(itens[jogador]))
+print('-=' * 15)
+
+if computador == 0: # PEDRA
+    if jogador == 0:
+        print('EMPATE')
+    elif jogador == 1:
+        print('JOGADOR VENCE')
+    elif jogador == 2:
+        print('COMPUTADOR VENCE')
+    else:
+        print('JOGADA INVÁLIDA!')
+
+elif computador == 1: # PAPEL
+    if jogador == 0:
+        print('COMPUTADOR VENCE')
+    elif jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('JOGADOR VENCE')
+    else:
+        print('JOGADA INVÁLIDA!')
+
+elif computador == 2: # TESOURA
+    if jogador == 0:
+        print('JOGADOR VENCE')
+    elif jogador == 1:
+        print('COMPUTADOR VENCE')
+    elif jogador == 2 :
+        print('EMPATE')
+    else:
+        print('JOGADA INVÁLIDA!')
