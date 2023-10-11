eebb0acf4c1a3b35d048d0ec3c2d3b66d4003878
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-10-11 10:38:17 -0300

    respostas

--- curso-python/Exercicio 50.py
+++ curso-python/Exercicio 50.py
@@ -1,2 +1,8 @@
 '''Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o'''
 
+soma_pares = 0
+for c in range(6):
+    numero = int(input('Digite um número inteiro: '))
+    if numero % 2 == 0:
+        soma_pares += numero
+print('A soma de todos os números pares foi {}'.format(soma_pares))
