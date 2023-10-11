349c3e757e295fdf98ea34db9e79d7632f4619d6
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-10-11 11:17:50 -0300

    Update Exercicio 46.py

--- curso-python/Exercicio 46.py
+++ curso-python/Exercicio 46.py
@@ -1,8 +1,8 @@
 '''Faça um programa que mostre na tela uma contagem REGRESSIVA para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles'''
 
 from time import sleep
-for c in range(10 , 0, -1):
-    sleep(1)
+for c in range(10, -1, -1):
+    sleep(0.5)
     print(c)
-    sleep(1)
+    sleep(0.5)
 print('FELIZ ANO NOVO!')
