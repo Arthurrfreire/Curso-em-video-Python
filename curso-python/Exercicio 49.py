abed06ff725f5bee1a0505d76020966c7be5d598
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-10-11 09:27:19 -0300

    update

--- curso-python/Exercicio 49.py
+++ curso-python/Exercicio 49.py
@@ -1,1 +1,7 @@
 '''Refaça o desafio 9, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for'''
+
+numero = int(input('Digite um número inteiro: '))
+for c in range(1, 11):
+    resultado = numero * c
+    print('{} x {} = {}'.format(numero, c, resultado))
+    
