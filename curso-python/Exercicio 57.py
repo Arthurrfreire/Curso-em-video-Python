4a8de378fc3387b244f3b0b7a5c1014d3f6d1904
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-10-12 15:53:15 -0300

    respostas

--- curso-python/Exercicio 57.py
+++ curso-python/Exercicio 57.py
@@ -1,1 +1,6 @@
-'''Faça um programa que leia o sexo de uma pessoa, mas só aceite ps valores 'M' e 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto'''
+'''Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' e 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto'''
+
+sexo = str(input('Informe seu sexo: [M/f] ')).strip().upper() [0]
+while sexo not in 'MmFf':
+    sexo = str(input('Dados inválidos. Por favor, informe seu sexo: ')).strip().upper() [0]
+print('Sexo {} registrado com sucesso'.format(sexo))
--- curso-python/Exercicio 61.py
+++ curso-python/Exercicio 61.py
@@ -1,1 +1,14 @@
 '''Refaça o desafio 051, lendo o primeiro termo e razão de uma PA, mostrando os 10 primeiro termos da progressão usando a estrutura while'''
+
+print('Gerador de PA')
+print('-=' * 10)
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print('{} -> '.format(termo), end='')
+    termo += razão
+    cont += 1
+print('FIM')
+    
