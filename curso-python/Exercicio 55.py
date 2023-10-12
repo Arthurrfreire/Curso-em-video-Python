0d5407a48e82e55b614adf9d9c35214c7f639ecc
Arthur Ramalho <arthur.ramalho.freire@gmail.com>
2023-10-12 09:43:56 -0300

    Update Exercicio 55.py

--- curso-python/Exercicio 55.py
+++ curso-python/Exercicio 55.py
@@ -5,7 +5,7 @@ maior = 0
 menor = 0
 for p in range(1, 6):
     peso = float(input('Digite o peso da {}ª: '.format(p)))
-    if p ==1:
+    if p == 1:
         maior = peso
         menor = peso
     else:
